90c5a97a6d5946dab8b1dc691ebb93c159d7d624
jason810496 <810496@email.wlsh.tyc.edu.tw>
2022-03-13 00:48:41 +0800

    add plot to fix bugs

--- 3_12/2-1-check.py
+++ 3_12/2-1-check.py
@@ -13,23 +13,35 @@ floor = box(length=20, height=0.01, width=10, color=color.green)  	#畫地板
 
 
 ball = sphere(radius = size, color=color.yellow, make_trail= True, trail_type="points", interval=100) 	#畫球
-ball.pos = vector(-10, size+0.1, 0)  
+ball.pos = vector(0, size+1, 0)  
 ball.v = vector(3, 10, 0)           
 
 ball_vpy = sphere(radius = size, color=color.green, make_trail= True, trail_type="points", interval=100) 	#畫球
-ball_vpy.pos = vector(-10, size+0.1, 0)
+ball_vpy.pos = vector(0, size+1, 0)
 ball_vpy.v = vector(3, 10, 0)
 
 ball_newton = sphere(radius = size, color=color.red, make_trail= True, trail_type="points", interval=100) 	#畫球
-ball_newton.pos = vector(-10, size+0.1, 0)
+ball_newton.pos = vector(0, size+1, 0)
 ball_newton.v = vector(3, 10, 0)
 
 dt = 0.001	#時間間隔 0.001 秒
 t = 0.0		#模擬初始時間為0秒
 
 damp = 0.2
+k=0.5
 
-while ball.pos.y > size or ball_vpy.pos.y > size or ball_newton.pos.y > size:    #模擬直到球落地 即y=球半徑
+xy_graph = graph(title=" XY plot" ,width=600,height=400 , x=100 ,y=10 ,xtitle="X" , ytitle="Y")
+
+xy_plot = gcurve( graph=xy_graph  , color=color.blue)
+
+xy_graph2 = graph(title=" XY plot newton" ,width=600,height=400 , x=100 ,y=10 ,xtitle="X" , ytitle="Y")
+
+xy_plot2 = gcurve( graph=xy_graph2  , color=color.red)
+
+# while ball.pos.y > size or ball_vpy.pos.y > size or ball_newton.pos.y > size:    #模擬直到球落地 即y=球半徑
+flag = True
+
+while t<3:
     rate(1/dt)    #每一秒跑 1000 次
     t = t + dt    #計時器
     
@@ -37,7 +49,6 @@ while ball.pos.y > size or ball_vpy.pos.y > size or ball_newton.pos.y > size:   
     
     ball.a = Fg/m 
     ball_vpy.a = Fg/m + AirRes/m
-    ball_newton.a = Fg/m
 
     ball.v = ball.v + ball.a*dt          #球的末速度 = 前一刻速度 + 加速度*時間間隔
     ball.pos = ball.pos + ball.v * dt    #球的末位置 = 前一刻位置 + 速度*時間間隔
@@ -46,8 +57,13 @@ while ball.pos.y > size or ball_vpy.pos.y > size or ball_newton.pos.y > size:   
     ball_vpy.pos = ball_vpy.pos + ball_vpy.v * dt 
     
     # newton
-    ball_newton.pos.x = ball_newton.v.x * (1 - exp(-damp*t))/damp
-    ball_newton.pos.y = - g*t/damp + (damp*ball_newton.v.y + g) * (1 - exp(-damp*t))/damp**2
+    if flag:
+        ball_newton.pos.x = (ball_newton.v.x * (1 - exp(-damp*t)) )/damp
+        ball_newton.pos.y = - g*t/damp + (damp*ball_newton.v.y + g) * (1 - exp(-damp*t))/damp**2
+
+    
+    xy_plot.plot(pos=(ball_vpy.pos.x,ball_vpy.pos.y))
+    xy_plot2.plot(pos=(ball_newton.pos.x,ball_newton.pos.y))
 
     # stop the ball 
     if ball.pos.y <= size:
@@ -57,6 +73,30 @@ while ball.pos.y > size or ball_vpy.pos.y > size or ball_newton.pos.y > size:   
         ball_vpy.v=vector(0,0,0)
 
     if ball_newton.pos.y <= size:
-        ball_newton.v=vector(0,0,0)
+        flag =False
 
 print (t, ball.v)
+
+
+
+'''
+xy_graph = graph(title=" XY plot" ,width=600,height=400 , x=100 ,y=10 ,xtitle="X" , ytitle="Y")
+
+xy_plot = gcurve( graph=xy_graph  , color=color.blue)
+
+xy_graph2 = graph(title=" XY plot" ,width=600,height=400 , x=100 ,y=10 ,xtitle="X" , ytitle="Y")
+
+xy_plot2 = gcurve( graph=xy_graph2  , color=color.blue)
+
+while t<5:
+
+    rate(1/dt) 
+    t = t + dt 
+
+    ball_newton.pos.x = (ball_newton.v.x * (1 - exp(-k*t)) )/k
+    ball_newton.pos.y = - g*t/k + (k*ball_newton.v.y + g) * (1 - exp(-k*t))/k**2
+
+    print(f"x:{ball_newton.pos.x} , y:{ball_newton.pos.y}")
+
+    xy_plot.plot(pos=(ball_newton.pos.x,ball_newton.pos.y))
+'''
