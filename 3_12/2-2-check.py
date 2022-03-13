68c62cb8a509b0f38566727a017fbfd70bbbb8a3
jason810496 <810496@email.wlsh.tyc.edu.tw>
2022-03-13 14:16:45 +0800

    finish 2-2-check

--- 3_12/2-2-check.py
+++ 3_12/2-2-check.py
@@ -1,3 +1,9 @@
+'''
+檢核作業 2-2：
+
+請完成視覺化的圓周圓周運動計時器，並在執行視窗紀錄每繞一圈所花的時間。執行畫面如下圖所示，物件視窗中當物體跨過原點時，執行視窗就會打印出時間。
+'''
+
 from vpython import *
 
 size = 0.1     #球的大小
@@ -13,24 +19,25 @@ ball.pos = vector(R,0,0)    #球的初始位置
 t = 0.0       #初始時間
 dt = 0.001    #時間間隔
 
-
-pre_pre_theta = 0
-pre_theta =0
-theta=0
+#等等要記錄的現在角度、前一角度和前前角度
+pre_pre_theta = 0	#前前角度
+pre_theta =0		#前一角度
+theta=0				#現在角度
 
 last_time=0
 while True:
     rate(1/dt)
 
-    # update theta
+    # 紀錄現在角度、前一角度和前前角度
     pre_pre_theta =pre_theta
     pre_theta = theta
     theta += omega*dt    #t時刻的角度
 
+	# 按照圓方程式中參數式的定義
     ball.pos = vector(R*cos(theta), R*sin(theta), 0)
-
+	# 按照講義提示的判斷方式判斷
     if pre_theta%(2*pi) > pre_pre_theta%(2*pi) and pre_theta%(2*pi) > theta%(2*pi):
         print(f"period : {t-last_time}")
         last_time=t
-        
+    #記得更新總時間
     t += dt             
