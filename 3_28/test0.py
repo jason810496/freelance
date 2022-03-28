0449832eca4f60c2562f1b7aa10d0df7f1412de8
jason810496 <810496@email.wlsh.tyc.edu.tw>
2022-03-28 19:43:11 +0800

    add comment

--- 3_28/test0.py
+++ 3_28/test0.py
@@ -14,44 +14,55 @@ fig,ax = plt.subplots()
 
 ScatterPlot = plt.scatter(x,y,c=RGBList)
 '''
+
+
+''' input method :
+
+X list , Y list , A list , B list  ,C list
+'''
+
 fig,ax = plt.subplots()
 
 df = pd.DataFrame(np.random.randint(0,50,size=(100, 5)), columns=['X','Y','a','b','c'])
-    # c = color style
-
-    # r:230 , g=b=range (max=150 , )
+ 
 
 RGBList = []
 
 
 for index, row in df.iterrows():  
     Sum  = row['a']+ row['b']+ row['c']
-    # RGBList.append( [230,row['RGB'],row['']] )
 
     Sum /= 150
     # 230/255 = 0.90196078431
+    # RGB range [0,1] (float )
     RGBList.append([0.90196078431 ,  Sum ,Sum ])
 
 
+# use dataframe directly 
 # df.plot.scatter(x='X', y='Y', c=RGBList , marker='s')
 
+# use plt.scatter ( marker = 's' -> change scatter plot shape )
 ScatterPlot = plt.scatter(df['X'],df['Y'],c=RGBList , marker='s')
 
 
+# add annotation on plot 
 annot = ax.annotate("", xy=(0,0), xytext=(20,20),textcoords="offset points",
                     bbox=dict(boxstyle="round", fc="w"),
                     arrowprops=dict(arrowstyle="->"),horizontalalignment='center', verticalalignment='top')
+
+# set unvisible 
 annot.set_visible(False)
 
 
 
+# update annotation by index 
+def update_annot(Detials):
 
-def update_annot(idx):
+    # print("Detials : ", idx) >>> {'ind': array([34], dtype=int32)}
+    # print("Detials : ", idx["ind"][0]) >>> 34
 
-    # print("idx : ", idx) >>> {'ind': array([34], dtype=int32)}
-    # print("idx : ", idx["ind"][0]) >>> 34
-
-    index = idx["ind"][0]
+    index = Detials["ind"][0]
+    # get position by ith index 
     pos = ScatterPlot.get_offsets()[ index ]
 
     # print(pos ) >>> [40.0 1.0] MEANS: [X,Y]
@@ -60,19 +71,27 @@ def update_annot(idx):
     # annot.get_bbox_patch().set_facecolor(cmap(norm(c[idx["idx"][0]])))
     annot.get_bbox_patch().set_alpha(0.4)
 
+# handle event 
 def HandleHover(event):
+    
+    # let annotation visible 
     vis = annot.get_visible()
     if event.inaxes == ax:
-        cont, idx = ScatterPlot.contains(event)
-        if cont:
-            update_annot(idx)
+
+        # Contains:bool  , Details:dictionary >>> {'ind': array([34], dtype=int32)} 
+        Contains, Detials = ScatterPlot.contains(event)
+
+        if Contains:
+            update_annot(Detials)
             annot.set_visible(True)
             fig.canvas.draw_idle()
         else:
             if vis:
+                # set unvisible 
                 annot.set_visible(False)
                 fig.canvas.draw_idle()
 
+# add mouse hover event listener
 fig.canvas.mpl_connect("motion_notify_event", HandleHover)
 
 plt.show()
