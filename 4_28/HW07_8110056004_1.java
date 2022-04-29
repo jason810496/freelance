ad55744d781f007c8768b1397020a74a11d81c6e
jason810496 <810496@email.wlsh.tyc.edu.tw>
2022-04-29 16:13:00 +0800

    finish

--- 4_28/HW07_8110056004_1.java
+++ 4_28/HW07_8110056004_1.java
@@ -8,11 +8,19 @@ public class HelloWorld{
 */
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
+import java.util.ArrayList;
 
 public class HelloWorld{
     public static void main(String[] args){
         int[][] test1 = {{1,1},{2,4},{2,10},{5,4},{4,8},{5,5},{8,4},{10,2},{10,1}} ;
-        Solution( test1 );
+        
+        int[][] test2 = {{1,1},{2,4},{2,10},{5,4},{4,8},{5,5},{8,4},{10,2},{10,1},{100,100}} ;
+        int[][] ans1 = Solution( test1 );
+        int[][] ans2 = Solution( test2 );
+        
+        System.out.println(Arrays.deepToString( ans1 ));
+        System.out.println(Arrays.deepToString( ans2 ));
     }
 	public static int[][] Solution(int[][] inputArr){ 
         // sort(inputArr, 0, inputArr.length);
@@ -20,17 +28,30 @@ public class HelloWorld{
         Arrays.sort( inputArr , new Comparator<int[]>() {
             @Override
             public int compare(int[] first, int[] second) {
-               if(first[0] > second[0]) return 1;
+               if(first[1] < second[1]) return 1;
                else return -1;
             }
         });
 
-        for(int i=0;i<inputArr.length;i++){
-            System.out.print( inputArr[i][0]);
-            System.out.print( " ");
-            System.out.println( inputArr[i][1]);
+        int n = inputArr.length , cur=0;
+        
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        for(int i=0;i<n;i++){
+            if( inputArr[i][0] > cur ){
+                cur = inputArr[i][0];
+                ans.add( Arrays.asList( inputArr[i][0] , inputArr[i][1]) );
+            }
         }
-        return new int[1][1];
+        
+        int[][] res = new int[ans.size()][2];
+ 
+        int i = 0;
+        for (List<Integer> l:ans) {
+            res[i  ][0] = l.get(0);
+            res[i++][1] = l.get(1);
+        }
+        
+        return res;
     }
    
 }
