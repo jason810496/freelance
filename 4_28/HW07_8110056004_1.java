cbbb1611dc7d26546fc4c69281a61ad252f2c936
jason810496 <810496@email.wlsh.tyc.edu.tw>
2022-04-29 16:05:45 +0800

    upd

--- 4_28/HW07_8110056004_1.java
+++ 4_28/HW07_8110056004_1.java
@@ -1,8 +1,41 @@
+/*
+public class HelloWorld{
 
-public abstract class Buy_Phone{
-	public abstract int[][] bestPhone(int[][] inputArr);
+     public static void main(String []args){
+        System.out.println("Hello World");
+     }
+}
+*/
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class HelloWorld{
+    public static void main(String[] args){
+        int[][] test1 = {{1,1},{2,4},{2,10},{5,4},{4,8},{5,5},{8,4},{10,2},{10,1}} ;
+        Solution( test1 );
+    }
+	public static int[][] Solution(int[][] inputArr){ 
+        // sort(inputArr, 0, inputArr.length);
+        
+        Arrays.sort( inputArr , new Comparator<int[]>() {
+            @Override
+            public int compare(int[] first, int[] second) {
+               if(first[0] > second[0]) return 1;
+               else return -1;
+            }
+        });
+
+        for(int i=0;i<inputArr.length;i++){
+            System.out.print( inputArr[i][0]);
+            System.out.print( " ");
+            System.out.println( inputArr[i][1]);
+        }
+        return new int[1][1];
+    }
+   
 }
 
+/*
 public  class HW07_8110056004_1 extends Buy_Phone{
 	public int[][] bestPhone(int[][] inputArr){ 
         sort(inputArr, 0, inputArr.length);
@@ -73,22 +106,4 @@ public  class HW07_8110056004_1 extends Buy_Phone{
         }
     }
 }
-/*
-Assignment Requirement:
- 
-Your program will take a two-dimensional integer array as input.
-Each element represents a smartphone model which contains two features (e.g., screen size and performance ranking).
-Please return an array containing a list of unbeatable smartphone models (In other words, filter out the phone models that can be beat by some model).
-Given two items A =(Xa, Ya) and B =(Xb, Yb), Item A beats Item B iff xa≥xb and ya≥yb
- 
- 
-[[1,1],[2,4],[2,10],[5,4],[4,8],[5,5],[8,4],[10,2],[10,1]]
-Input:
- 
-Output: [[2,10],[4,8],[5,5],[8,4],[10,2]]
-
-[[1,1],[2,4],[2,10],[5,4],[4,8],[5,5],[8,4],[10,2],[10,1],[100,100]]
-Input:
- 
-Output: [[100,100]]
 */
