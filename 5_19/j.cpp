116d0975818e8590e795453888aa50280f33345c
jason810496 <810496@email.wlsh.tyc.edu.tw>
2022-05-20 17:03:15 +0800

    to hard

---
+++ 5_19/j.cpp
@@ -0,0 +1,37 @@
+
+#pragma GCC optimize("O2")
+#include<bits/stdc++.h>
+using namespace std;
+#define OAO cin.tie(0);ios_base::sync_with_stdio(0);
+#define F first
+#define S second
+#define ll long long 
+#define D long double
+#define range(x) x.begin(),x.end()
+#define MEM(x,i) memset(x,i,sizeof(x))
+#define rep(i,n) for(int i=0;i<n;++i)
+#define REP(i,st,n) for(int i=st;i<=n;++i)
+#define PB emplace_back
+typedef pair<int,int> pii;
+const int MAX_N = 1e5+5;
+const int INF = 1e9;
+
+ll T[MAX_N<<1];
+
+
+
+signed main(){
+    OAO
+    int n ; cin>>n ;
+
+    int q; cin>>q;
+
+    while( q-- ){
+        int L , R ; cin>>L>>R;
+
+        
+    }
+
+    return 0;
+}
+
