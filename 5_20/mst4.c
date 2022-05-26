4a7c61c31482228a5ee9fd92bf868b598a4bb073
jason810496 <810496@email.wlsh.tyc.edu.tw>
2022-05-26 12:51:10 +0800

    build the correct Graph !!!

--- 5_20/mst4.c
+++ 5_20/mst4.c
@@ -1,147 +1,77 @@
+/*
+Prim's Algorithm：
+Prim's algorithm works by attaching a new edge to a single growing tree at each step:
+Start with any vertex as a single-vertex tree; then add V-1 edges to it,
+always taking next the minimum-weight edge that connects a vertex on the tree to a vertex not yet on the tree.
+
+以下註解為參考，實際可依照自己邏輯完成 
+
+*/
+
+
+//完成.....  代表缺少一行關鍵
+
+/*
+  完成.....
+  代表須完成此區塊
+*/
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<limits.h>
+#define MAX 1000000
 
-typedef struct Pair Pair;
-typedef struct List List;
-typedef struct PQ PQ;
+typedef struct node node;
 typedef struct Graph Graph;
+typedef struct mst mst;
 
-struct Pair{
-    int vertex , wt;
+struct node
+{
+  int vertex;
+  int cost;
+  struct node *next;
 };
 
-
-struct List{
-    int Size,idx;
-    Pair *arr;
+struct Graph
+{
+  node **adjLists;
 };
 
-void List_Init(List *self){
-    self->idx=0;
-    self->Size=1;
-    self->arr = NULL;
-}
+int Size = 0 ;
 
-void List_Resize(List *self,int n){
-    self->arr = (Pair*)malloc( n* sizeof(Pair ));
-    self->Size = n ;
-}
-
-int List_Empty(List *self){
-    return self->Size;
-}
-
-void List_Push(List *self,Pair e){
-    self->arr[ self->idx++ ] = e;
-    self->Size++;
-}
-
-void List_Pop(List *self ){
-    self->idx--;
-}
-
-struct Graph{
-    List *adj;
-    int *visited;
-    int Size;
+struct  mst{
+	int	start;
+	int	end;
+	int	cost;
 };
 
+mst pq[MAX];//Priority Queue
+int pqIndex = 0;
+int *visited;
 
-/*  Graph */
-void Graph_Init(Graph *G,int n){
-
-    G->adj = (List*)malloc( n*sizeof(List));
-    for(int i=0;i<n;i++){
-        Resize( &G->adj[i] , n);
-    }
-
-    G->visited = (int*)malloc( n*sizeof(int) );
-    for (int i = 0; i < n; i++){
-	    G->visited[i] = 0;
-	}
-
-    G->Size = n ;
-}
-
-void addEdge(Graph *G,int u, int v ,int wt){
-    Pair e1;
-    e1.vertex = v;
-    e1.wt = wt;
-    List_Push( &G->adj[u] , e1 );
-
-    Pair e2;
-    e2.vertex = u;
-    e2.wt = wt;
-    List_Push( &G->adj[v] , e2 );
-}
+//Prim's Algorithm using adjacency list
+node *createNode(int,int);
+Graph *graphInit(int);
+void addEdge(Graph *graph, int, int ,int);
+int getSize(Graph *graph, int);
+node *getNode(Graph *graph, int, int);
+void visit(Graph *graph, int);
+void prim(Graph *graph, int);
 
 
-/* PQ */
-struct PQ{
-    int Size;
-    List arr;
-};
-
-inline int par(int i ){
-    return (i-1)/2;
-}
-
-inline int lc(int i){
-    return i<<1 + 1;
-}
-
-inline int rc(int i){
-    return i<<1 + 2;
-}
-
-inline int PQ_Size(PQ *self){
-    return self->Size;
-}
-
-void Heap_Up(PQ *self,int i){
-    
-    if( i && self->arr.arr[ par(i) ].wt > self->arr.arr[i].wt ){
-        swap( self->arr.arr[i] , self->arr.arr[ par(i) ] );
-
-        Heap_Up( self , par(i) );
-    }
-}
-
-void Heap_Down(PQ *self ,int i ){
-    int l = lc(i) , r = rc(i) ;
-    int largest = i;
-
-    if( l<= self->arr.idx && self->arr.arr[ l ].wt < self->arr.arr[ i ].wt ){
-        largest = l;
-    }
-    if( r<=self->arr.idx && self->arr.arr[ r ].wt < self->arr.arr[ i ].wt ){
-        largest = i;
-    }
-
-    if( largest!=i ){
-        swap( self->arr.arr[i] , self->arr.arr[largest] );
-        Heap_Down( self , largest );
-    }
-}
-
-void PQ_Push(PQ *self,Pair e){
-    List_Push( &self->arr , e );
-
-    Heap_Up(self , self->arr.idx);
-}
-
-Pair PQ_Top(PQ *self){
-    return self->arr.arr[0];
-}
-
-void PQ_Pop(PQ *self){
-    self->arr.arr[0] = self->arr.arr[ self->arr.idx-1 ];
-    List_Pop(&self->arr);
-    Heap_Down(self,0);
-}
-
-/* Data Structure Finish */
+/*
+Priority Queue Operations 
+此處操作上課說過，
+每次加入新頂點後須要額外花費O(E)的時間找到最小成本的邊，
+請將此處的處理方法改善成僅需(log V)的版本
+(demo時必問，未改寫者滿分80)
+*/
+int isEmpty();
+int isFull();
+void enqueue(mst e);
+int peek();
+mst dequeue();
+void display();
 
 
 
@@ -149,7 +79,8 @@ int main(int argc, char *argv[])
 {
 
 	int i, n, m;
-
+	mst *e;
+	mst *ptr;
 	FILE* fin;
 	
 	if (argc < 1) {
@@ -165,28 +96,53 @@ int main(int argc, char *argv[])
 	fscanf(fin, "%d, %d", &n, &m);
 	printf("nodes = %3d, edges = %3d\n", n, m);
 	
+	e = (mst *)malloc(sizeof(mst)*(m+1));
+    Graph *graph = graphInit(n);
 
-    Graph graph;
-    Graph_Init(&graph,n);
-
+	// Graph size
+	Size = n;
 
 	int st , ed , wt ;
 	for (i = 1; i <= m; i++) {
+		ptr = &(e[i]);
 		fscanf(fin, "%d, %d, %d", &st, &ed , &wt); //從txt讀入邊的資料
 		printf("edge[%3d]: start = %3d, end = %3d, cost = %3d\n", i, st, ed , wt );//將讀入的邊印出
 		
-        addEdge( &graph , st , ed , wt );
-        addEdge( &graph , ed , st , wt );
+        addEdge( graph , st , ed , wt );
+        addEdge( graph , ed , st , wt );
+
+        // debug
+        printf("debug : \n");
+
+        for(int i=0;i<n;i++){
+            node *adj = graph->adjLists[i];
+            printf( " %d , sz %d : " ,i,getSize(graph,i));
+            for(int j=0;j<getSize(graph,i) ;j++){
+                printf( "%d ", adj->vertex );
+                adj = adj->next;
+            }
+            printf("\n");
+        }
+
 	}
+
+ 	visited = (int *)malloc(sizeof(int) * n);
+
+	for (i = 0; i < n; i++)
+	{
+	    visited[i] = 0;
+	}
+
+	// debug
 
 	printf("debug : \n");
 
 	for(int i=0;i<n;i++){
-		List adj = graph.adj[i];
-		printf( " %d : " ,i);
-		// printf( " size : %d\n" ,getSize(&graph,i) );
-		for(int j=0;j<adj.idx ;j++){
-			printf( "%d ", adj.arr[j] );
+		node *adj = graph->adjLists[i];
+		printf( " %d , sz %d : " ,i,getSize(graph,i));
+		for(int j=0;j<getSize(graph,i) ;j++){
+			printf( "%d ", adj->vertex );
+			adj = adj->next;
 		}
 		printf("\n");
 	}
@@ -194,9 +150,213 @@ int main(int argc, char *argv[])
     //prim gogo~
 	for (i = 0; i < n ;i++)
 	{
-		// if(!visited[i]) prim(&graph, i);
+		if(!visited[i]) prim(graph, i);
 	}	
     fclose(fin);
     return 0;
 }
 
+
+Graph *graphInit(int vertices)
+{
+	// fix malloc error
+	Graph *graph = (Graph*)malloc(sizeof(Graph));
+	graph->adjLists = (node**)malloc(vertices * sizeof(node *));
+	int i;
+	for (i = 0; i < vertices; i++)
+	{
+    	graph->adjLists[i] = NULL;
+	}
+	return graph;
+}
+
+void Create(node **cur , int v ,int c){
+    if( ! *cur ){
+        *cur = (node*)malloc(sizeof(node));
+        (*cur)->cost = c;
+        (*cur)->vertex = v;
+        (*cur)->next = NULL;
+
+        // return cur;
+
+        return;
+    }
+
+    Create( &(*cur)->next ,v,c);
+}
+void addEdge(Graph *graph, int i, int j,int cost)
+{
+	// node *newNode = 
+	
+
+	node **cur = &graph->adjLists[i];
+	
+    Create( cur,j,cost);
+	// while( temp ){
+	// 	temp = graph->adjLists[i]->next;
+    //     printf("t\n");
+	// }
+    // printf("create\n");
+	// // *cur = createNode(j, cost);
+
+    // temp = (node*)malloc(sizeof(node));
+	// (temp)->vertex = j;
+	// (temp)->cost = cost;
+	// (temp)->next = NULL;
+
+	// newNode->next = cur ;
+}
+
+node *createNode(int v,int cost)
+{
+	node *newNode = (node*)malloc(sizeof(node));
+	newNode->vertex = v;
+	newNode->cost = cost;
+	newNode->next = NULL;
+
+	return newNode;
+}
+
+int getSize(Graph *graph, int v)
+{
+	int count = 0;
+	node *temp = (node*)malloc(sizeof(node));
+    temp = graph->adjLists[v];
+	while(temp)
+	{
+		/*
+		完成尋訪串列並紀錄此串列的長度
+		*/
+
+		temp= temp->next;
+		count++;
+	}
+	return count;
+}
+
+node *getNode(Graph *graph, int v, int index)
+{
+	int count = 0;
+	node *temp = graph->adjLists[v];
+	while (count < index)
+	{
+	    temp = temp->next;
+	    count++;
+	}
+	//完成 將找到的頂點回傳
+
+	return temp ;
+}
+
+void visit(Graph *graph, int v) {
+    /*
+	將選擇的頂點標記，
+	並將此頂點相鄰的邊加入pq
+	*/
+	visited[v] = 1;
+	int j;
+	int m = getSize(graph, v);
+	for (j = 0; j < m; j++)
+	{
+        node *vv = getNode(graph, v, j);
+        if (!visited[j]) //完成此判斷句
+        {
+            mst e = {v, vv->vertex, vv->cost};
+           //完成將此條邊加入pq
+
+		   enqueue( e );
+        }
+    }
+}
+
+void prim(Graph *graph, int v)
+{
+	printf("\ngrowing spanning trees...\n");
+	visit(graph, v);
+    int weight;
+	while (!isEmpty())
+	{
+		mst e = dequeue();
+        int v = e.start, w = e.end; 
+        if (visited[v] && visited[w]) continue; 
+        printf("start = %3d, end = %3d, cost = %3d\n", e.start, e.end, e.cost);//將選擇的最小成本邊印出
+		
+		//完成成本加總
+		
+        if (!visited[v]) visit(graph, v);
+        if (!visited[w]) visit(graph, w);
+	}
+    printf("total cost: %d ", weight);//印出此生成樹總成本
+}
+
+int isEmpty(){
+	
+	return pqIndex<=0 ;
+}
+
+int isFull(){
+
+	return pqIndex>= (Size-1) ;
+}
+
+void enqueue(mst e)
+{
+    if(!isFull())
+    {
+        pqIndex++;
+        pq[pqIndex].start = e.start;
+		pq[pqIndex].end = e.end;
+		pq[pqIndex].cost = e.cost;
+    }
+}
+
+int peek()
+{
+    int minCost = MAX;
+    int edge = -1;
+	
+    for (int i = 0; i <= pqIndex; i++) 
+    { 
+        if (minCost == pq[i].cost && edge > -1) // choose bigger edge
+        {
+            minCost = pq[i].cost;
+            edge = i;
+        }
+        else if (minCost > pq[i].cost) {
+            minCost = pq[i].cost;
+            edge = i;
+        }
+    }
+    
+    return edge;
+}
+
+mst dequeue()
+{
+	mst e;
+    if(!isEmpty())
+    {
+        int edge = peek();
+		
+        e.start = pq[edge].start;
+        e.end = pq[edge].end;
+        e.cost = pq[edge].cost;
+        for (int i = edge; i < pqIndex; i++) {
+            pq[i] = pq[i + 1];
+        }
+		//完成從pq移除最小邊
+
+		pqIndex--;
+    }
+
+	return e;
+}
+
+/*
+void display(){
+    for (int i = 0; i <= pqIndex; i++) 
+    {
+        printf("(%d %d %d)\n",pq[i].start,pq[i].end,pq[i].cost);
+    } 
+}
+*/
