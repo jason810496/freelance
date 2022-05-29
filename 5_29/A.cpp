489bf593b9f80f8d017a11fd227e60777cb93e20
jason810496 <810496@email.wlsh.tyc.edu.tw>
2022-05-29 16:51:23 +0800

    Trie V1

--- 5_29/A.cpp
+++ 5_29/A.cpp
@@ -14,25 +14,26 @@ struct IStringDatabase {
 // ---------------------------------------------
 
 // [YOUR CODE WILL BE PLACED HERE]
-
-struct Node{
-
-    std::vector< Node > Child(60);
-    bool isEnd=false , has=false;
-
-    Node(){
-        Child.resize(60);
-        isEnd = false;
-        has = false;
-    }
-}
+#define Alphabet_Size 60
+/*
+    Trie: Prefix Tree
+    Application : words auto-complete using Trie
+*/
+struct node{
+    bool is_end;
+    int cnt;
+    node *child[Alphabet_Size];
+    node(){
+        is_end=false;
+        cnt=0;
+        for(int i=0;i<Alphabet_Size;i++) child[i]=nullptr;
+    };
+};
 
 class StringDatabase : public IStringDatabase
 {
-	//std::list<std::string> _list ;
-	// std::vector<std::string> _vec ;
 
-    Node root;
+    node *root = new node();
 
 public :
 	void Add(const std::string&) ;
@@ -40,25 +41,73 @@ public :
 	std::vector<std::string> StartsWith(const std::string& prefix) ;
 	~StringDatabase();
 
-    void Insert(struct Node*rt ,const std::string &str);
+    void Insert(node *rt,const std::string &str);
+    std::vector< std::string> Search(node *rt,const std::string &str);
+
 };
 
 
 
 // ----------------------------------------
 
+void Insert(node *rt,const std::string &str){
+    node *temp = rt;
 
+    for(char ch :str ){
+        int index= ch-'a';
 
-std::vector<std::string> Search( struct Node*rt ,const std::string &str){
-    struct Node *temp = rt;
+        if(temp->child[index]==nullptr){
+            temp->child[index]=new node;
+        }
 
-    
+        temp= temp->child[index];
+    }
+
+    temp->is_end= true;
 }
 
-void StringDatabase::Add(const std::string& a )
-{
-   
+void DFS(node *rt, std::string &cur, std::vector< std::string> &ans){
+    if( rt->is_end ){
+        ans.push_back( cur );
+    }
 
+    for(int i=0;i<Alphabet_Size;i++){
+        if( rt->child[i] ){
+            cur.push_back( char(i) );
+            DFS(rt->child[i] ,cur,ans );
+            cur.pop_back();
+        }
+    }
+
+}
+
+std::vector<std::string> Search(node *rt,const std::string &str){
+    node *temp = rt;
+
+    bool flag = true ; 
+    for(char ch:str){
+        int idx = ch='a';
+        if( temp->child[ idx ]==nullptr ){
+            flag = false;
+        }
+        temp = temp->child[ idx ];
+    }
+
+    
+
+    std::vector< std::string > ans;
+
+    if( !flag ) return ans ;
+
+    std::string word = str;
+
+    DFS(temp,word,ans);
+
+    return ans ;
+}
+
+void StringDatabase::Add(const std::string& a ){
+    Insert(root,a);
 }
 
 bool StringDatabase::Remove(const std::string& a )
@@ -66,30 +115,44 @@ bool StringDatabase::Remove(const std::string& a )
 	
 }
 
-std::vector<std::string> StringDatabase::StartsWith(const std::string& prefix)
-{
-	std::vector<std::string> ret ;
-	// 是否重複
-	int left  ;
-	int k ; 
-	binary_search( _vec , 0 , _vec.size() - 1 , prefix , left );
-	for( ; left < _vec.size() ; ++left )
-	{
-		const std::string &ss = _vec[left] ;
-
-		if( strstr( ss.c_str() , prefix.c_str() ) != NULL )
-			ret.push_back( ss );
-		else
-			break ;
-
-		
-	}
-
-	return ret ; 
-
-
+std::vector<std::string> StringDatabase::StartsWith(const std::string& prefix){
+    return Search(root,prefix);
 }
 
 StringDatabase::~StringDatabase()
 {
+}
+
+
+
+// =============================
+
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& v) {
+    os << '[';
+    if (v.size() > 0) {
+        os << v[0];
+        for (size_t i = 1; i < v.size(); ++i) {
+            os << ", " << v[i];
+        }
+    }
+    return os << ']';
+}
+
+int main(){
+
+    StringDatabase db;
+    db.Add("Helloworld");
+    db.Add("HelloWorld");
+    db.Add("Hello");
+    db.Add("Helloworld");
+    db.Add("Hello");
+    db.Add("Hi");
+
+    std::cout << "01) " << db.StartsWith("Hello") << std::endl;
+    std::cout << "02) " << db.StartsWith("hello") << std::endl;
+    std::cout << "03) " << db.StartsWith("H") << std::endl;
+
+    return 0;
 }
