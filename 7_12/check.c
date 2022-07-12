0ed3ea24ce3bbeec9279aa48262b64b5e93e14c8
jason810496 <810496@email.wlsh.tyc.edu.tw>
2022-07-12 08:54:33 +0800

    multi ans

---
+++ 7_12/check.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+int main(){
+    
+    int k =10 ;
+
+    do {
+        k--;
+        printf("%d" , k);
+    }while( k>0 );
+
+    // while( k=10 ){
+    //     k--;
+    //     printf("%d" , k );
+    // }
+    return 0;
+}
