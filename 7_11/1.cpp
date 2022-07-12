95564c863a3a7a37711a27dfc28ff073cc6f3d7f
jason810496 <810496@email.wlsh.tyc.edu.tw>
2022-07-12 08:22:16 +0800

    Finish

--- 7_11/1.cpp
+++ 7_11/1.cpp
@@ -6,12 +6,17 @@ int main(){
     int cur , sum=0 ,cnt=0;
     printf("Q1 input:\n");
 
-    while( scanf("%d", &cur) && cur!=-99 ){
+    while( scanf("%d", &cur) ){
+        if( cur == -99 ) break;
         sum+=cur;
         cnt++;
     }
 
     printf("Q1 output:\n");
+    if( cnt==0 ){
+        printf("invalid testcase\n");
+        return 0;
+    }
     printf("sum = %d\n average = %f\n" , sum  , (float)sum/(float)cnt);
     return 0;
 }
