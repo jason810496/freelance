b72cfe5b1bbef4c7ab5711b2170321b8d0528e9a
jason810496 <810496@email.wlsh.tyc.edu.tw>
2022-07-17 17:53:40 +0800

    AVL Tree init

---
+++ 7_17/AVL/AVLTree.h
@@ -0,0 +1,32 @@
+
+// AVL Node Clas
+class AVLTreeNode {
+public:
+	// Should have attributes named:
+	// parent - pointer to parent
+	// left - pointer to left child
+	// right - pointer to right child
+	// key - node's key
+	// value - node's value
+	// height - node's height
+
+	// Constructors ...
+};
+
+// AVL Tree class
+class AVLTree
+{
+private:
+	// Tree attributes go here
+	// Your tree MUST have a root node named root
+
+public:
+
+
+	
+
+
+	void* getRoot() const { return root; }; // DO NOT REMOVE
+};
+
+// AVL Tree Methods go here
