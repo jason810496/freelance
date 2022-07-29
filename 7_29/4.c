d43c378e9c93dd1129eb56a5b8dbf64ea9881669
jason810496 <810496@email.wlsh.tyc.edu.tw>
2022-07-29 10:53:03 +0800

    Finish 4.c

---
+++ 7_29/4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int max(int a,int b){
+    return (a>b ? a:b);
+}
+
+int left(int i){
+    return i*2+1;
+}
+
+int right(int i){
+    return i*2+2;
+}
+
+int height(int *T ,int cur, int n){
+    if( cur > n ) return 0;
+    if( T[cur]==-1 ) return 0;
+    
+    return max( height(T,left(cur),n) , height(T,right(cur),n) )+1;
+}
+int main(){
+
+    int n;
+    scanf("%d" , &n);
+
+    int T[n];
+
+    // -1 stand for null 
+    for(int i=0;i<n;i++){
+        scanf("%d" , &T[i] );
+    }
+
+    for(int i=0;i<n;i++){
+        printf("%d " , T[i] );
+    }
+
+    printf("\nTree height : %d" , height(T , 0, n ) );
+    return 0;
+}
