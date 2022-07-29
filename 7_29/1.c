2d1c97eef9eff149678bc11995fc4c5196a904be
jason810496 <810496@email.wlsh.tyc.edu.tw>
2022-07-29 09:54:36 +0800

    Finish 1.c

---
+++ 7_29/1.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node node;
+
+struct node{
+    int val;
+    struct node*L, *R;
+};
+
+void UI(){
+    printf("Enter data to be inserted or type -1 for no insertion : ");
+}
+void UI_L(int v){
+    printf("Enter left child of : %d\n" ,v);
+}
+void UI_R(int v){
+    printf("Enter right child of : %d\n" ,v);
+}
+
+void build(node **root){
+    UI();
+    int v;
+    scanf("%d" , &v );
+    (*root)->val = v;
+    if( v==-1 ) return ;
+    
+    (*root)->L = (node*)malloc(sizeof(node));
+    (*root)->R = (node*)malloc(sizeof(node));
+    
+    UI_L( v );
+    build(&(*root)->L);
+
+    UI_R( v );
+    build(&(*root)->R);
+}
+
+void inorder(node *root){
+    if( !root || root->val==-1 ) return;
+
+    inorder( root->L );
+    printf("%d " , root->val );
+    inorder( root->R );
+}
+
+int main(){
+
+    int flag = 1 ;
+    node *root;
+
+    build(&root); 
+
+    printf("inorder traversal:\n");
+    inorder(root);   
+    
+    return 0;
+}
